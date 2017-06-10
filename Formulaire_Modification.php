948c8666c781b81e4b88e68fe20eee7288975a3f
Lily <Lily@10.18.9.213>
2017-06-10 12:36:55 +0200

    pas fini

--- Formulaire_Modification.php
+++ Formulaire_Modification.php
@@ -1,62 +1,41 @@
 <?php
-$Etu_Numero = $_POST['numetu'];
-$UV_Numero = $_POST['numUV'];
+include('BibliothequePHP.php');
+
 ?>
 
 <html>
 <head>
-        <title>Modfier un cursus</title>
-        <link type='text/CSS' href='../include/CSS/style.js'>
+        <title>Modifier un cursus</title>
+        <link type='text/CSS' href='include/CSS/style.js'>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-    <form method="POST" id='FormMoficationCursus' name='FormMoficationCursus' action="Formulaire_Modification_Action.php">
-         <?php for ($i = 1; $i <= $UV_Numero; $i++) {
-    echo ("UV N°".$i." -");
-    echo (" <div>N° du Semestre : <input type='text' name='numsem' size='10' maxlength='10' value='' /></div>
-                    <div>Label du Semestre :<input type='text' name='labelsem' size='10' maxlength='10' value='' /></div>
-                    <div>Sigle :<input type='text' name='sigle' size='5' maxlength='4' value='' /></div>
-                    <div>Catégorie :<select name='categorie'>
-                    <option>CS</option>
-                    <option>TM</option>
-                    <option>ME</option>
-                    <option>EC</option>
-                    <option>HT</option>
-                    <option>NPML</option>
-                    <option>ST</option>
-                    </select></div>
-                    <div>Affectation :");
-           
-/* <input type="radio" name='affectation' value='TC'/>Tronc Commun
-            <input type="radio" name='affectation' value='TCBR'/>Tronc Commun de Branche
-            <input type="radio" name='affectation' value='FCBR'/>Branche
-                <div>A-t-elle a été passé à l'UTT ?
-            <input type="radio" name='inUTT' value='true'/>Oui
-            <input type="radio" name='inUTT' value='false'/>Non
-                <div>Correspond-t-elle a votre profil ?
-            <input type="radio" name='inProfil' value='true'/><u>Oui</u>
-            <input type="radio" name='inProfil' value='false'/><u>Non</u>
-                <div>Nombre de crédit obtenu :<input type='number' name='numcredit' size='5' maxlength='4' value='' /></div>
-                <div>Résultat à l'UV :><select name='result'>
-                    <option>A</option>
-                    <option>B</option>
-                    <option>C</option>
-                    <option>D</option>
-                    <option>E</option>
-                    <option>FX</option>
-                    <option>F</option>
-                </select></div>
-            <div><button type='button' value="Ajouter une UV" id="AjoutElement"></button></div>
-                </fieldset>
-            
+
+    <table>
+        <form method="POST" id='ModifCursus' name='ModifCursus' action="Modification_Cursus_Action.php">
+    <div id='NumEtu'>Numéro de l'étudiant: <input type='number' name='numetu' size='10' maxlength='10' value='' /></div>    
+  
+    <div id='ListeUV'>            
+    <div id='UV1'>
+    <fieldset>
+            <div>N° du Semestre : <input type='text' name='numsem[]' size='10' maxlength='10' value='' /></div>
+            <div>Label du Semestre :<input type='text' name='labelsem[]' size='10' maxlength='10' value='' /></div>
+            <div>Sigle :<input type='text' name='sigle[]' size='5' maxlength='4' value='' /></div>
+            <div>Catégorie :<?php echo inputSelect($Categorie_Options,"categorie[]");?></div>
+            <div>Affectation :<?php echo inputSelect($Affectation_Options,"affectation[]");?></div>
+            <div>A-t-elle a été passé à l'UTT ?
+                <?php echo inputSelect($Boolean_Options,"inUTT[]"); ?></div>
+            <div>Correspond-t-elle a votre profil ?
+                <?php echo inputSelect($Boolean_Options,"inProfil[]"); ?></div>
+            <div>Nombre de crédit obtenu :<input type='number' name='numcredit[]' size='5' maxlength='4' value='' /></div>
+            <div>Résultat à l'UV :<?php echo inputSelect($Resultat_Options,"result[]"); ?></div>
+    </fieldset>
+    </div>
+    </div>
         <div><a href="Visualisation_Cursus.php"><input type='submit' value='Envoyer' /></a></div>
-        <div><input type='reset' value='Annuler' /></div
-        </form>"*/
-            }
-            ?>         
-        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
-        <script type="text/JavaScript" src="include/javascript/AjoutElement.js"></script>
-        </table>
-    </body>
+        <div><input type='reset' value='Réinitialiser' /></div
+        </form>        
+    </table>
+</body>
 </html>
